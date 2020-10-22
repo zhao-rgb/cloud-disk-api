da25056c8888a1d99907fc5d2a9cca54d9d62b8c
zhao-rgb <1019916061@qq.com>
2020-10-22 14:22:26 +0800

    egg.js项目初始化搭建

---
+++ app/controller/home.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx } = this;
+    ctx.body = 'hello world';
+  }
+
+  async list() {
+    this.ctx.body = {
+      msg: 'ok',
+      data: [
+        {
+          name: '微服务',
+          price: 100,
+        },
+        {
+          name: 'Java',
+          price: 88,
+        },
+        {
+          name: 'JavaScript',
+          price: 77,
+        },
+      ],
+    };
+  }
+}
+
+module.exports = HomeController;
+
